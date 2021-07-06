bad1c8515e7bb9ed9edb6f63ec20aefe8d5f46d3
chaosotter <meiss.mark@gmail.com>
2021-07-06 10:34:31 -0700

    Converted to interpolated 16-bit samples.

--- python/Ms. Tracky/main.py
+++ python/Ms. Tracky/main.py
@@ -9,27 +9,27 @@ def main():
     m.print_summary()
 
     p = pyaudio.PyAudio()
-    stream = p.open(format=pyaudio.paInt8, channels=1, rate=44100, output=True)
+    stream = p.open(format=pyaudio.paInt16, channels=1, rate=44100, output=True)
 
-    samp = m.samples[0]
-    print('Sample: {}'.format(samp))
-    stream.write(samp.data)
+    for samp in m.samples:
+        print('Sample: {}'.format(samp))
+        #stream.write(samp.data)
 
-    notes = [214, 170, 143]  # C4, E4, G4
-    for n in notes:
-        scale = 44100.0 / 8287.14  # extend samples this many units for middle C
-        extend = scale * (n / 214.0)  # extend samples this many units for this note
-        print(scale, extend)
+        notes = [214, 170, 143]  # C4, E4, G4
+        for n in notes:
+            scale = 44100.0 / (2 * 8287.14)  # extend samples this many units for middle C
+            extend = scale * (n / 214.0)  # extend samples this many units for this note
+            print(scale, extend)
 
-        data = bytearray()
-        rem = 0.0
-        for s in samp.data:
-            rem += extend
-            while rem > 1:
-                data.append(s)
-                rem -= 1
+            data = bytearray()
+            rem = 0.0
+            for s in samp.data:
+                rem += extend
+                while rem > 1:
+                    data.append(s & 0xff)
+                    data.append((s & 0xff00) >> 8)
+                    rem -= 1
 
-        for x in range(10):
             stream.write(bytes(data))
 
     stream.stop_stream()
--- python/Ms. Tracky/sample.py
+++ python/Ms. Tracky/sample.py
@@ -18,8 +18,25 @@ class Sample:
         self.data = None
 
     def store(self, data):
-        """Stores the actual sample data for later use."""
-        self.data = data
+        """Stores the actual sample data for later use.
+
+        The input data are signed 8-bit values.  We upscale to 16 bits and
+        double the number of samples through simple linear interpolation.
+
+        For convenience in mixing, the upconverted sample is stored as an
+        array of ints rather than a bytearray.
+        """
+        self.data = []
+        for i in range(len(data) - 1):
+            a = 256 * (data[i] if data[i] < 128 else data[i] - 256)
+            b = 256 * (data[i+1] if data[i+1] < 128 else data[i+1] - 256)
+            self.data.append(a)
+            self.data.append((a + b) // 2)
+
+        if len(data):
+            last = 256 * data[-1] if data[-1] < 128 else data[-1] - 256
+            self.data.append(last)
+            self.data.append(last)
 
     def __str__(self):
         """Provides a human-readable one-line summary."""
