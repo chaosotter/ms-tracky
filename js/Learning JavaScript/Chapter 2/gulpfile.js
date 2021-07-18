10efcba64cad534c72d88458c3f07fb218397361
chaosotter <meiss.mark@gmail.com>
2021-07-18 14:58:18 -0700

    Got the linting nightmare straight.

--- js/Learning JavaScript/Chapter 2/gulpfile.js
+++ js/Learning JavaScript/Chapter 2/gulpfile.js
@@ -1,8 +1,11 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
-// Gulp dependencies go here.
+const eslint = require('gulp-eslint');
 
 gulp.task('default', async function() {
+    gulp.src(["es6/**/*.js", "public/es6/**/*.js"])
+        .pipe(eslint())
+        .pipe(eslint.format());
     // Node source.
     gulp.src('es6/**/*.js')
         .pipe(babel())
